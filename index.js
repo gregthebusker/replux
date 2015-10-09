d0fa608c26023fb387390dd00071d0783e3efeae
Greg Schechter <diabolomaster@gmail.com>
2015-10-09 10:15:51 +0200

    Export functions

---
+++ index.js
@@ -0,0 +1,9 @@
+var Utilities = require('src/Utilities.js');
+var Creator = require('src/Creator.js');
+var connector = require('src/connector.js');
+
+module.exports = {
+    ...Utilities,
+    Creator,
+    connector,
+};
